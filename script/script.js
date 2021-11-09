f9ad063c0545814490e7d7e60d7ea4fffff15ec5
Jampa Matos <jp.coutm@gmail.com>
2021-11-09 20:07:08 -0300

    Done basic operations and the operator function.

---
+++ script/script.js
@@ -0,0 +1,32 @@
+function sum (a, b){
+    return a + b;
+}
+
+function subtract (a,b){
+    return a - b;
+}
+
+function multiply (a,b){
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+function operate (op, n, m){
+    switch(op){
+        case '+':
+            return sum(n,m);
+            break;
+        case '-':
+            return subtract(n,m);
+            break;
+        case '*':
+            return multiply(n,m);
+            break;
+        case '/':
+            return divide(n,m);
+            break;
+    }
+}
