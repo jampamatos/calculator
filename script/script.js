70b94e1ed7b69a65ac17e4d3f6db3803cced9395
Jampa Matos <jp.coutm@gmail.com>
2021-11-10 19:22:47 -0300

    Finished calculator styling.

--- script/script.js
+++ script/script.js
@@ -1,32 +1,32 @@
-function sum (a, b){
+function sum(a, b) {
     return a + b;
 }
 
-function subtract (a,b){
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a,b){
+function multiply(a, b) {
     return a * b;
 }
 
-function divide (a,b) {
+function divide(a, b) {
     return a / b;
 }
 
-function operate (op, n, m){
-    switch(op){
+function operate(op, n, m) {
+    switch (op) {
         case '+':
-            return sum(n,m);
+            return sum(n, m);
             break;
         case '-':
-            return subtract(n,m);
+            return subtract(n, m);
             break;
         case '*':
-            return multiply(n,m);
+            return multiply(n, m);
             break;
         case '/':
-            return divide(n,m);
+            return divide(n, m);
             break;
     }
 }
